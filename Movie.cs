bdb94a111c2f338e951bfc02f8ca72d0ec9daa3d
Joe Nelson <jnelson136@my.wctc.edu>
2024-03-07 14:39:52 -0600

    [1.17] Add quotes if it contains a comma

--- Movie.cs
+++ Movie.cs
@@ -1,7 +1,21 @@
 public class Movie
 {
     public UInt64 movieId { get; set; }
-    public string title { get; set; }
+
+    // private field
+    string _title;
+    public string title
+    {
+        get
+        {
+            return this._title;
+        }
+        set
+        {
+            // if there is a comma(,) in the title, wrap it in quotes
+            this._title = value.IndexOf(',') != -1 ? $"\"{value}\"" : value;
+        }
+    }
     public List<string> genres { get; set; }
     // constructor
     public Movie()
